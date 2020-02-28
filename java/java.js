50a3a1e7d49dd728f0f13352d7e3ca30efbe0f51
George-Stephen <yazz777stevie@gmail.com>
2020-02-28 09:37:11 +0300

    new

--- java/java.js
+++ java/java.js
@@ -1,3 +1,31 @@
 $("document").ready(function(){
+ $(".Neo").click(function(){
+     $(".1").slideToggle();
+ })
+ $(".sic").click(function(){
+    $(".2").slideToggle();
+})
+$(".det").click(function(){
+    $(".3").slideToggle();
+}) 
+$(".chic").click(function(){
+    $(".4").slideToggle();
+})
+$(".greek").click(function(){
+    $(".5").slideToggle();
+})
+$(".louis").click(function(){
+    $(".6").slideToggle();
+})
+$(".Nyc").click(function(){
+    $(".7").slideToggle();
+})
+$(".cali").click(function(){
+    $(".8").slideToggle();
+})
+ function calculate(){
+     //*Business logic*//
+     var person =
 
+ }
 });
