6139363fe43f548579c7e8b142e2c8c487d5a173
George-Stephen <yazz777stevie@gmail.com>
2020-02-28 16:32:39 +0300

    new data

--- java/java.js
+++ java/java.js
@@ -23,22 +23,49 @@ $(".Nyc").click(function(){
 $(".cali").click(function(){
     $(".8").slideToggle();
 })
- function calculate(){
-     //*Business logic*//
-     var person = function(names,number,location,Email){
-        this.names=names;
-        this.number=number;
-        this.location=location;
-        this.Email=Email;
-    }
-     var order = function(size,toppings,crust,number){
-         this.size=size;
-         this.toppings=toppings;
-         this.crust=crust;
-         this.number=number;
-     }
-
-
-
+ //*Business logic*//
+ function person (names,number,location,Email){
+    this.names=names;
+    this.number=number;
+    this.location=location;
+    this.Email=Email;
+}
+  function order (size,toppings,crust,number,delivery){
+     this.size=size;
+     this.toppings=toppings;
+     this.crust=crust;
+     this.number=number;
+     this.delivery=delivery;
  }
-});
+//*User logic*//
+var names =$(".name").val();
+var number=$(".number").val();
+var location=$(".location").val();
+var email =$(".Email").val();
+var topping_price=300;
+var User = new person(names,number,location,email);
+
+
+var size = $(".size").val();
+var toppings =$(".toppings").val();
+var crust = $(".crust").val();
+var number =$(".number").val();
+var delivery=$(".delivery").val();
+var Myorder = new order(size,toppings,crust,number,delivery);
+let price;
+ 
+  $(".calc").click(function(){
+switch(size){
+    case size="Large":
+        price=1100;
+        break
+     case  size="Medium":
+         price=800;
+         break
+     case size="small":
+         price = 500;
+         break
+}
+    alert(price)
+  });
+})
