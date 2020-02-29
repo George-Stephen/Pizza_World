0ae7b2a18338ec73312318f3ac96eeef4cd11f05
George-Stephen <yazz777stevie@gmail.com>
2020-02-29 20:21:50 +0300

    finally

--- java/java.js
+++ java/java.js
@@ -1,10 +1,10 @@
 $("document").ready(function(){
-    function order(type,size,toppings,number,delivery){
-        this.type = type;
+    function order(size,toppings,number,delivery,type){
         this.size = size;
         this.toppings =toppings;
         this.number = number;
         this.delivery = delivery;
+        this.type = type;
     }
     order.prototype.price = function(){
         let price =0;
@@ -25,27 +25,18 @@ $("document").ready(function(){
             Total = Total * this.number;
         }
         else alert("Enter your sizes ")
-        order.prototype.update = function(){
-            if(this.delivery === "home-delivered"){
-                Total = Total + 300 ;
-            }
-            else 
-            if(this.delivery === "personal"){
-                Total = Total + 0 ;
-            }
-            else
-            alert("Please choose your delivery")
-        }
         $("#details").text("Your pizza will be ready shorty,The total price is "+Total);
+
+        
     }
+    
      $(".calc").click(function(){
-         event.preventDefault();
+         var type = $("#type").val()
          var size= $("#size").val();
          var toppings = $(".toppings").val();
          var number = $(".number").val();
          var delivery = $(".delivery").val();
-          var myOrder = new order(size,toppings,number,delivery);
+          var myOrder = new order(size,toppings,number,delivery,type);
           myOrder.price()
-     }); 
-     $()  
+     });   
 })
